0e4122c355963ad4e82bc61df74e70eb4ae69dd9
Benjy <benjyw@gmail.com>
2013-05-15 17:00:33 -0400

    WIP

--- src/python/twitter/pants/reporting/html_reporter.py
+++ src/python/twitter/pants/reporting/html_reporter.py
@@ -14,10 +14,52 @@ from twitter.pants.reporting.reporter import Reporter
 from twitter.pants.reporting.reporting_utils import list_to_report_element
 
 
+# A regex to recognize substrings that are probably URLs or file paths. Broken down for readability.
+_PREFIX = r'(https?://)?/?' # http://, https:// or / or nothing.
+_REL_PATH_COMPONENT = r'(\w|[-.])+'  # One or more alphanumeric, underscore, dash or dot.
+_ABS_PATH_COMPONENT = '/' + _REL_PATH_COMPONENT
+_ABS_PATH_COMPONENTS = '(%s)+' % _ABS_PATH_COMPONENT
+_OPTIONAL_TARGET_SUFFIX = '(:%s)?' % _REL_PATH_COMPONENT  # For /foo/bar:target.
+
+# Note that we require at least two path components.
+# We require no trailing dots because some tools print an ellipsis after file names
+# (I'm looking at you, zinc). None of our files end in a dot in practice, so this is fine.
+_PATH = _PREFIX + _REL_PATH_COMPONENT + _ABS_PATH_COMPONENTS + _OPTIONAL_TARGET_SUFFIX + '\w'
+_PATH_RE = re.compile(_PATH)
+
+def linkify(buildroot, s):
+  """Augment text by heuristically finding URL and file references and turning them into links/"""
+  def to_url(m):
+    if m.group(1):
+      return m.group(0)  # It's an http(s) url.
+    path = m.group(0)
+    if path.startswith('/'):
+      path = os.path.relpath(path, buildroot)
+    else:
+      # See if it's a reference to a target in a BUILD file.
+      # TODO: Deal with sibling BUILD files?
+      parts = path.split(':')
+      if len(parts) == 2:
+        putative_dir = parts[0]
+      else:
+        putative_dir = path
+      if os.path.isdir(os.path.join(buildroot, putative_dir)):
+        path = os.path.join(putative_dir, BuildFile._CANONICAL_NAME)
+    if os.path.exists(os.path.join(buildroot, path)):
+      return '/browse/%s' % path
+    else:
+      return None
+
+  def maybe_add_link(url, text):
+    return '<a target="_blank" href="%s">%s</a>' % (url, text) if url else text
+  return _PATH_RE.sub(lambda m: maybe_add_link(to_url(m), m.group(0)), s)
+
+
 class HtmlReporter(Reporter):
   """HTML reporting to files.
 
-  The files are intended to be served by the ReportingServer, not accessed directly from the filesystem.
+  The files are intended to be served by the ReportingServer,
+  not accessed directly from the filesystem.
   """
 
   def __init__(self, run_tracker, html_dir, template_dir):
@@ -60,7 +102,7 @@ class HtmlReporter(Reporter):
     # Create the template arguments.
     workunit_dict = workunit.to_dict()
     if workunit_dict['cmd']:
-      workunit_dict['cmd'] = self._linkify(workunit_dict['cmd'].replace('$', '\\\\$'))
+      workunit_dict['cmd'] = linkify(self._buildroot, workunit_dict['cmd'].replace('$', '\\\\$'))
     args = { 'indent': len(workunit.ancestors()) * 10,
              'html_path_base': self._html_path_base,
              'workunit': workunit_dict,
@@ -193,44 +235,6 @@ class HtmlReporter(Reporter):
       }
     return self._renderer.render_name('output', args)
 
-  # We require no trailing dots because some tools print an ellipsis after file names
-  # (I'm looking at you, zinc). None of our files end in a dot in practice, so this is fine.
-
-  # At least two path components.
-
-  # A regex to recognize things that are probably URLs or file paths.
-  #_PREFIX = r'(https?://)?/?' # http://, https:// or /.
-  #_PATH_COMPONENT = r'/(?:\w|[-.])+'  # One or more alphanumeric, underscore, dash or dot characters.
-  #_PATH = _PREFIX + _COMPONENT + r'(?:/(?:\w|[-.])+)+(:w|[-.]+)?\w'
-  path_re = re.compile(r'(https?://)?/?(?:\w|[-.])+(?:/(?:\w|[-.])+)+(:w|[-.]+)?\w')
-
-  def _linkify(self, s):
-    """Augment text by heuristically finding URL and file references and turning them into links/"""
-    def to_url(m):
-      if m.group(1):
-        return m.group(0)  # It's an http(s) url.
-      path = m.group(0)
-      if path.startswith('/'):
-        path = os.path.relpath(path, self._buildroot)
-      else:
-        # See if it's a reference to a target in a BUILD file.
-        # TODO: Deal with sibling BUILD files?
-        parts = path.split(':')
-        if len(parts) == 2:
-          putative_dir = parts[0]
-        else:
-          putative_dir = path
-        if os.path.isdir(putative_dir):
-          path = os.path.join(putative_dir, BuildFile._CANONICAL_NAME)
-      if os.path.exists(os.path.join(self._buildroot, path)):
-        return '/browse/%s' % path
-      else:
-        return None
-
-    def maybe_add_link(url, text):
-      return '<a target="_blank" href="%s">%s</a>' % (url, text) if url else text
-    return HtmlReporter.path_re.sub(lambda m: maybe_add_link(to_url(m), m.group(0)), s)
-
   def _render_callable(self, template_name, arg_string, outer_args):
     rendered_arg_string = self._renderer.render(arg_string, outer_args)
     inner_args = dict([(k, v[0]) for k, v in urlparse.parse_qs(rendered_arg_string).items()])
@@ -240,7 +244,7 @@ class HtmlReporter(Reporter):
 
   def _htmlify_text(self, s):
     colored = self._handle_ansi_color_codes(cgi.escape(s))
-    return self._linkify(colored).replace('\n', '</br>')
+    return linkify(self._buildroot, colored).replace('\n', '</br>')
 
   # Replace ansi color sequences with spans of appropriately named css classes.
   ansi_color_code_re = re.compile(r'\033\[((?:\d|;)*)m')
