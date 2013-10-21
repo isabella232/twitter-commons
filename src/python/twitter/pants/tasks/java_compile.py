a8cc317bd5f3cce2dfad4861a03c64c5c3eedc13
Patrick Lawson <pl@foursquare.com>
2013-10-21 16:01:36 -0400

    Move JavaCompile over to bootstrap_classpath
    Auditors: benjy

--- src/python/twitter/pants/tasks/java_compile.py
+++ src/python/twitter/pants/tasks/java_compile.py
@@ -23,6 +23,7 @@ from twitter.common.dirutil import safe_open, safe_mkdir
 
 from twitter.pants import has_sources, is_apt, Task
 from twitter.pants.base.target import Target
+from twitter.pants.binary_util import bootstrap_classpath
 from twitter.pants.goal.workunit import WorkUnit
 from twitter.pants.tasks import TaskError
 from twitter.pants.tasks.jvm_compiler_dependencies import Dependencies
@@ -98,8 +99,8 @@ class JavaCompile(NailgunTask):
     self._depfile_dir = os.path.join(workdir, 'depfiles')
     self._deps = Dependencies(self._classes_dir)
 
-    self._jmake_profile = context.config.get('java-compile', 'jmake-profile')
-    self._compiler_profile = context.config.get('java-compile', 'compiler-profile')
+    self._external_tools = context.config.getlist('java-compile', 'external-tools')
+    self._compiler_bootstrap_tools = context.config.getlist('java-compile', 'compiler-bootstrap-tools')
 
     self._opts = context.config.getlist('java-compile', 'args')
     self._jvm_args = context.config.getlist('java-compile', 'jvm_args')
@@ -243,15 +244,18 @@ class JavaCompile(NailgunTask):
     return sources, Target.identify(targets)
 
   def compile(self, classpath, sources, fingerprint, depfile):
-    jmake_classpath = self.profile_classpath(self._jmake_profile)
-
+    jmake_classpath = bootstrap_classpath(self._external_tools,
+                                          context=self.context,
+                                          java_runner=self.runjava_indivisible)
     opts = [
       '-classpath', ':'.join(classpath),
       '-d', self._classes_dir,
       '-pdb', os.path.join(self._classes_dir, '%s.dependencies.pdb' % fingerprint),
     ]
 
-    compiler_classpath = self.profile_classpath(self._compiler_profile)
+    compiler_classpath = bootstrap_classpath(self._compiler_bootstrap_tools,
+                                             context=self.context,
+                                             java_runner=self.runjava_indivisible)
     opts.extend([
       '-jcpath', ':'.join(compiler_classpath),
       '-jcmainclass', 'com.twitter.common.tools.Compiler',
@@ -260,8 +264,12 @@ class JavaCompile(NailgunTask):
     opts.extend(map(lambda arg: '-C%s' % arg, self._javac_opts))
 
     opts.extend(self._opts)
-    return self.runjava_indivisible(_JMAKE_MAIN, classpath=jmake_classpath, opts=opts, args=sources,
-                                    jvmargs=self._jvm_args, workunit_name='jmake',
+    return self.runjava_indivisible(_JMAKE_MAIN,
+                                    classpath=jmake_classpath,
+                                    opts=opts,
+                                    args=sources,
+                                    jvmargs=self._jvm_args,
+                                    workunit_name='jmake',
                                     workunit_labels=[WorkUnit.COMPILER])
 
   def check_artifact_cache(self, vts):
