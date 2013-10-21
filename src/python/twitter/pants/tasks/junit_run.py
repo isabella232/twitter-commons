41a47a751411d4c6eead833419ceec6cd90a9c50
Patrick Lawson <pl@foursquare.com>
2013-10-21 16:24:20 -0400

    Move junit to bootstrap_classpath
    Auditors: benjy

--- src/python/twitter/pants/tasks/junit_run.py
+++ src/python/twitter/pants/tasks/junit_run.py
@@ -21,6 +21,7 @@ import sys
 from twitter.common.dirutil import safe_mkdir, safe_open
 
 from twitter.pants import binary_util, is_codegen, is_java, is_scala, is_test, junit_tests
+from twitter.pants.binary_util import bootstrap_classpath
 from twitter.pants.goal.workunit import WorkUnit
 from twitter.pants.tasks import Task, TaskError
 from twitter.pants.tasks.jvm_task import JvmTask
@@ -139,8 +140,8 @@ class JUnitRun(JvmTask):
     context.products.require_data('exclusives_groups')
 
     self.confs = context.config.getlist('junit-run', 'confs')
-    self.junit_profile = context.config.get('junit-run', 'junit_profile')
-    self.emma_profile = context.config.get('junit-run', 'emma_profile')
+    self._bootstrap_tools = context.config.getlist('junit-run', 'bootstrap-tools')
+    self._emma_bootstrap_tools = context.config.get('junit-run', 'emma-bootstrap-tools')
 
     self.java_args = context.config.getlist('junit-run', 'args', default=[])
     if context.options.junit_run_jvmargs:
@@ -207,9 +208,12 @@ class JUnitRun(JvmTask):
       tests = list(self.normalize_test_classes() if self.test_classes
                                                  else self.calculate_tests(targets))
       if tests:
-        junit_classpath = self.classpath(binary_util.profile_classpath(self.junit_profile,
-                            workunit_factory=self.context.new_workunit), confs=self.confs,
-                            exclusives_classpath=self.get_base_classpath_for_target(targets[0]))
+        bootstrapped_cp = bootstrap_classpath(self._bootstrap_tools,
+                                              context=self.context,
+                                              workunit_factory=self.context.new_workunit)
+        junit_classpath = self.classpath(bootstrapped_cp,
+                                         confs=self.confs,
+                                         exclusives_classpath=self.get_base_classpath_for_target(targets[0]))
 
         def run_tests(classpath, main, jvmargs=None):
           def test_workunit_factory(name, labels=list(), cmd=''):
@@ -236,7 +240,7 @@ class JUnitRun(JvmTask):
             raise TaskError()
 
         if self.coverage:
-          emma_classpath = binary_util.profile_classpath(self.emma_profile)
+          emma_classpath = bootstrap_classpath(self._emma_bootstrap_tools, context=self.context)
 
           def instrument_code():
             safe_mkdir(self.coverage_instrument_dir, clean=True)
